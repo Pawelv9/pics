28989973e55ee5a6b406eefbb12eb485f1e6391f
Pavel <pawelv9@gmail.com>
2024-04-25 22:39:10 +0300

    updated searchImages

--- src/api.js
+++ src/api.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
    headers: {
     Authorization: 'Client-ID 3G30SR-rJm0wj8_wcKp_Cehxu03X6zY_LUOAlAx5Dt8'
    },
    params: {
-    query: 'cars'
+    query: term
    } 
   })
-
-  console.log(response)
-
-  return response;
+  
+  return response.data.results;
 }
 
 export default searchImages;
